59045c5405fcc2ed3c31aa16de1e8875c4466f15
fpuluguzzu <ashapuluguzzu@gmail.com>
2023-03-30 14:39:39 -0600

    created Data Displaying & Directives

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  imageSrc: string = "https://upload.wikimedia.org/wikipedia/commons/9/92/Open_book_nae_02.svg";
+  showPreview = false;
+  constructor() { }
+  checkpreview() {
+    this.showPreview = !this.showPreview;
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
+
+
